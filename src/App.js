03c5f65db05f98e36aba87408ad642138fb1fcd9
Ccolina03 <colinaperu2003@gmail.com>
2023-04-04 16:26:12 -0600

    Connected the three pages using Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react"
-import PostalLookup from "./Postal/PostalLookup"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Universities from './Universities/Universities';
+import Home from './Home/Home';
+import PostalLookup from './Postal/PostalLookup';
 
 const App = () => {
   return (
-   <div>
-    <PostalLookup/>
-   </div>
-  )
-}
+    <Router>
+    <Routes>
+      <Route path="/carlos_colina_launchpad-react-code-challenge" element={<Home />} />
+      <Route path="/carlos_colina_launchpad-react-code-challenge/universities" element={<Universities />} />
+      <Route path="/carlos_colina_launchpad-react-code-challenge/postal-lookup" element={<PostalLookup />} />
+    </Routes>
+  </Router>
+  );
+};
 
 export default App;
--- src/Postal/components/TabBar.js
+++ src/Postal/components/TabBar.js
@@ -1,26 +1,24 @@
 import React from "react";
-//import { NavLink} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const TabBar = () => {
-  return (
-    <header className="border-b sticky top-0 z-20 bg-black">
-      <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
-        <span className="text-xl font-bold text-white">Vog Challenge</span>
-        <div className="space-x-5">
-          <div to="/carlos_colina_launchpad-react-code-challenge/" end className="text-lg pt-1 font-light text-white">
-            Home
-          </div>
-          <div
-            to="/carlos_colina_launchpad-react-code-challenge/universities"
-            end
-            className="text-lg pt-1 font-light text-white"
-          >
-            Universities
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
+    return (
+        <header className='border-b sticky top-0 z-20 bg-black'>
+            <div className='flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl'>
+                <span className='text-xl font-bold text-white'>
+                    Vog Challenge
+                </span>
+                <div className="space-x-5">
+                    <Link to="/carlos_colina_launchpad-react-code-challenge/" className="text-lg pt-1 font-light text-white">
+                        Home
+                    </Link>
+                    <Link to="/carlos_colina_launchpad-react-code-challenge/postal-lookup" className="text-lg pt-1 font-light text-white">
+                        Postal lookup
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
+}
 
 export default TabBar;
